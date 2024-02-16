79237d61818a9aaee43710f9ee3cec7d79b23e49
Sergey Markov <107784915+SrgMarkov@users.noreply.github.com>
2024-02-16 09:09:49 +0400

    add cart

--- seller_bot.py
+++ seller_bot.py
@@ -44,6 +44,76 @@ def get_products_keyboard(api_auth):
     keyboard.append([InlineKeyboardButton("Моя корзина", callback_data="my_cart")])
     return InlineKeyboardMarkup(keyboard)
 
+
+def get_or_create_cart(user_id, api_auth):
+    carts_response = requests.get(
+        "http://localhost:1337/api/carts", headers=api_auth, timeout=60
+    )
+    carts_response.raise_for_status()
+    for cart in carts_response.json()["data"]:
+        if cart["attributes"]["tg_id"] == user_id:
+            return cart["id"]
+    payload = {"data": {"tg_id": user_id}}
+    created_cart = requests.post(
+        "http://localhost:1337/api/carts/", headers=api_auth, json=payload, timeout=60
+    )
+    return created_cart.json()["data"]["id"]
+
+
+def show_cart(api_auth, cart_id):
+    carts_response = requests.get(
+        f"http://localhost:1337/api/carts/{cart_id}?populate=cart_products",
+        headers=api_auth,
+        timeout=60,
+    )
+    if carts_response.status_code == 404:
+        return "В корзину пока не добавлено ни одного товара"
+
+    cart_text = []
+    cart_buttons = []
+    summary_cost = 0
+    for product in carts_response.json()["data"]["attributes"]["cart_products"]["data"]:
+        product_id = product['id']
+        cart_products = requests.get(
+            f"http://localhost:1337/api/cart-products/{product_id}?populate=fish_shop",
+            headers=api_auth,
+            timeout=60,
+        )
+        cart_products.raise_for_status()
+        product_in_cart = cart_products.json()["data"]["attributes"]
+        product_quantity = product_in_cart["quantity"]
+        product = product_in_cart["fish_shop"]["data"]["attributes"]
+        product_position_cost = product_quantity * product["price"]
+        summary_cost += product_position_cost
+        cart_buttons.append((product["title"], product_id))
+        cart_text.append(
+            f"{product['title']}\nцена за кг. - {product['price']}р.\n{product_quantity}кг. в корзине за {product_position_cost}р.\n\n"
+        )
+    cart_text = (
+        f"{''.join(cart_text)}\n Итоговая стоимость - {round(summary_cost, 2)}р."
+    )
+    return cart_text, cart_buttons
+
+
+def get_cart_keyboard(buttons):
+    cart_keyboard = [
+        [InlineKeyboardButton(f"удалить {button}", callback_data=number)]
+        for button, number in buttons
+    ]
+    cart_keyboard.append([InlineKeyboardButton("Оформить заказ", callback_data="purchase")])
+    cart_keyboard.append([InlineKeyboardButton("В меню", callback_data="back")])
+    return InlineKeyboardMarkup(cart_keyboard)
+
+
+def start(update: Update, context: CallbackContext):
+    context.user_data["chat_id"] = update.message.chat_id
+    update.message.reply_text(
+        "Товары в наличии:",
+        reply_markup=get_products_keyboard(context.user_data.get("request_headers")),
+    )
+    return "HANDLE_MENU"
+
+
 def start(update: Update, context: CallbackContext):
     context.user_data["chat_id"] = update.message.chat_id
     update.message.reply_text(
@@ -58,6 +128,15 @@ def get_product_info(update: Update, context: CallbackContext):
     query.answer()
     request_headers = context.user_data.get("request_headers")
     context.user_data["product_id"] = query.data
+    if query.data == "my_cart":
+        cart_text, cart_buttons = show_cart(
+            context.user_data.get("request_headers"),
+            get_or_create_cart(str(query.message.from_user.id), request_headers),
+        )
+        query.message.reply_text(
+            cart_text, reply_markup=get_cart_keyboard(cart_buttons)
+        )
+        return "CART"
 
     product = get_product_details(query.data, request_headers)
     picture_url = requests.get(
@@ -93,6 +172,63 @@ def get_back_product_list(update: Update, context: CallbackContext):
             "Товары в наличии:", reply_markup=get_products_keyboard(request_headers)
         )
         return "HANDLE_MENU"
+    if query.data == "to_cart":
+        if not context.user_data.get("cart_id"):
+            context.user_data["cart_id"] = get_or_create_cart(
+                str(query.message.from_user.id), request_headers
+            )
+        products_in_cart = requests.get(
+            "http://localhost:1337/api/cart-products?populate=fish_shop",
+            headers=request_headers,
+            timeout=60,
+        )
+        products = products_in_cart.json()["data"]
+        for product in products:
+            quantity = product["attributes"]["quantity"] + 1
+            if int(context.user_data.get("product_id")) == int(
+                product["attributes"]["fish_shop"]["data"]["id"]
+            ):
+                payload = {"data": {"quantity": quantity}}
+                requests.put(
+                    f"http://localhost:1337/api/cart-products/{product['id']}",
+                    json=payload,
+                    headers=request_headers,
+                    timeout=60,
+                )
+                query.message.reply_text(
+                    "Хотите заказать что то ещё?",
+                    reply_markup=get_products_keyboard(request_headers),
+                )
+                return "HANDLE_MENU"
+
+        payload = {
+            "data": {
+                "quantity": 1,
+                "fish_shop": {"connect": [int(context.user_data.get("product_id"))]},
+                "carts": {"connect": [context.user_data.get("cart_id")]},
+            }
+        }
+        requests.post(
+            "http://localhost:1337/api/cart-products/",
+            headers=request_headers,
+            json=payload,
+            timeout=60,
+        )
+
+        query.message.reply_text(
+            "Хотите заказать что то ещё?",
+            reply_markup=get_products_keyboard(request_headers),
+        )
+        return "HANDLE_MENU"
+    if query.data == "my_cart":
+        cart_text, cart_buttons = show_cart(
+            request_headers,
+            get_or_create_cart(str(query.message.from_user.id), request_headers),
+        )
+        query.message.reply_text(
+            cart_text, reply_markup=get_cart_keyboard(cart_buttons)
+        )
+        return "CART"
 
 
 def handle_users_reply(update: Update, context: CallbackContext):
@@ -123,6 +259,7 @@ def handle_users_reply(update: Update, context: CallbackContext):
     states_functions = {
         "START": start,
         "HANDLE_MENU": get_product_info,
+        "HANDLE_DESCRIPTION": get_back_product_list,
     }
     state_handler = states_functions[user_state]
 
