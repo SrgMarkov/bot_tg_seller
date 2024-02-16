94d1f013aad0d6e4eeb92875dda4a420f6b09444
Sergey Markov <107784915+SrgMarkov@users.noreply.github.com>
2024-02-16 15:16:20 +0400

    add crm schema to repo

---
+++ api/fish-shop/controllers/fish-shop.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * fish-shop controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::fish-shop.fish-shop');
---
+++ api/fish-shop/routes/fish-shop.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * fish-shop router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::fish-shop.fish-shop');
